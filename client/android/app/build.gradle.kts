9e0dc1f3ec9a611fc8c4eaf5bfceed17ea9d8ab3
Andrew R <beta@runelite.bot>
2026-02-06 20:27:35 -0600

    release: v2.0.1

--- client/android/app/build.gradle.kts
+++ client/android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.newaeven.game"
         minSdk = 24
         targetSdk = 34
-        versionCode = 18
-        versionName = "2.0.0"
+        versionCode = 19
+        versionName = "2.0.1"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
